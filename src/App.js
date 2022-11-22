b5a8f1993c5122d8044e6eede627bd86012c2e1e
Ysquid <ahk648@gmail.com>
2022-11-22 12:29:16 -0600

    intial commit, halfway through tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import validator from 'validator'
 
 function App() {
+  const [signupInput, setSignupInput] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState('')
+
+  const handleChange = (e) => {
+    setSignupInput({
+      ...signupInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!validator.isEmail(signupInput.email)) {
+      return setError('the email you input is invalid')
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email Addres
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-control"
+            value={signupInput.email}
+            onChange={handleChange}
+          ></input>
+
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-control"
+            value={signupInput.password}
+            onChange={handleChange}
+          ></input>
+
+          <label htmlFor="confirm-password" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirmPassword"
+            className="form-control"
+            value={signupInput.confirmPassword}
+            onChange={handleChange}
+          ></input>
+        </div>
+        {error && <p>{error}</p>}
+        <button type="submit" 
+        className="btn btn-primary" 
+        onClick={handleClick}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,63 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   //1) Rendering the component that we want to test
+//   render(<App />);
+
+//   //2)Finding a specific element within the component
+//   //Using the screen.getByText method with a Regular Expression
+//   const linkElement = screen.getByText(/learn react/i);
+
+//   //3) Assertion - expect(ourElement).toBeSomething
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("inputs should be initially empty", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  const passwordInputElement = screen.getByLabelText("Password");
+  const passwordConfirmInputElement =
+    screen.getByLabelText(/confirm password/i);
+  expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(passwordConfirmInputElement.value).toBe("");
+});
+
+test("should be able to type an email", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  userEvent.type(emailInputElement, "selena@gmail.com");
+  expect(emailInputElement.value).toBe("selena@gmail.com");
+});
+
+test("should be able to type a password", () => {
+  render(<App />);
+  const passwordInputElement = screen.getByLabelText("Password");
+  userEvent.type(passwordInputElement, "swordfish");
+  expect(passwordInputElement.value).toBe("swordfish");
+});
+
+test("should be able to type a confirmation password", () => {
+  render(<App />);
+  const passwordConfirmInputElement =
+    screen.getByLabelText(/confirm password/i);
+  userEvent.type(passwordConfirmInputElement, "swordfish");
+  expect(passwordConfirmInputElement.value).toBe("swordfish");
+});
+
+test("should show email error message on invalid email", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  const submitButtonElement = screen.getByRole("button", { name: /submit/i });
+  //first time we call this element, its null as we haven't submitted invalid yet
+  const emailErrorElement = screen.queryByText(/the email you input is invalid/i);
+  expect(emailErrorElement).not.toBeInTheDocument
+
+  userEvent.type(emailInputElement, "selenagmail.com");
+  userEvent.click(submitButtonElement)
+  //now we call it again in a new variable, after we have performed the submit action
+  const emailErrorElementAgain = screen.queryByText(/the email you input is invalid/i);
+  expect(emailErrorElementAgain).toBeInTheDocument()
 });
