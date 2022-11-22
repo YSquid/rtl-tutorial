207942f3f051ff23e196c9efbb422862c30b9815
Ysquid <ahk648@gmail.com>
2022-11-22 15:16:33 -0600

    final cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -54,11 +54,11 @@ test("1) inputs should be initially empty", () => {
   expect(confirmPasswordInputElement.value).toBe("");
 });
 
+//the first call does 2 things
+//creates the const emailInputElement which equals the returned object.emailInputElement value
+//calls the function with argument {email: "selena@gmail.com"} passed in
+//result is we clal the function, the if passes for email so types that in, and returns emailInputElement to our local variable from the returned value of the function
 test("2) should be able to type an email", () => {
-  //this call does 2 things
-  //creates the const emailInputElement which equals the returned object.emailInputElement value
-  //calls the function with argument {email: "selena@gmail.com"} passed in
-  //result is we clal the function, the if passes for email so types that in, and returns emailInputElement to our local variable from the returned value of the function
   const { emailInputElement } = typeIntoForm({
     email: "selena@gmail.com",
   });
@@ -82,48 +82,41 @@ test("4) should be able to type a confirmation password", () => {
 
 test("5) should show email error message on invalid email", () => {
   //first time we call this element, its null as we haven't submitted invalid yet
-  const emailErrorElement = screen.queryByText(
-    /the email you input is invalid/i
-  );
-  expect(emailErrorElement).not.toBeInTheDocument;
+  expect(screen.queryByText(/the email you input is invalid/i)).not
+    .toBeInTheDocument;
   typeIntoForm({ email: "selenagmail.com" });
   submitForm();
-  //now we call it again in a new variable, after we have performed the submit action
-  const emailErrorElementAgain = screen.queryByText(
-    /the email you input is invalid/i
-  );
-  expect(emailErrorElementAgain).toBeInTheDocument();
+  //now we call it again, after we have performed the submit action
+  expect(
+    screen.queryByText(/the email you input is invalid/i)
+  ).toBeInTheDocument();
 });
 
 test("6) should show password error if password is less than 5 characters", () => {
+  //setup
   typeIntoForm({ email: "selena@gmail.com", password: "word" });
   submitForm();
 
-  //get value of error elements that render after submit (or not render in case of emailErrorElement)
-  const emailErrorElement = screen.queryByText(
-    /the email you input is invalid/i
-  );
-  const passwordErrorElement = screen.queryByText(
-    /the password you entered should contain 5 or more characters/i
-  );
-
   //assertions - no emailErrorElement in doc, passwordErrorElement in doc
-  expect(emailErrorElement).not.toBeInTheDocument();
-  expect(passwordErrorElement).toBeInTheDocument();
+  expect(
+    screen.queryByText(/the email you input is invalid/i)
+  ).not.toBeInTheDocument();
+  expect(
+    screen.queryByText(
+      /the password you entered should contain 5 or more characters/i
+    )
+  ).toBeInTheDocument();
 });
 
 test("7) confirm password should match password", () => {
-  //setup
   //actions - make PW not match
-  // userEvent.type(emailInputElement, "selena@gmail.com");
-  // userEvent.type(passwordInputElement, "swordfish");
-  // userEvent.type(confirmPasswordInputElement, "sword");
   const { passwordInputElement, confirmPasswordInputElement } = typeIntoForm({
     email: "selena@gmail.com",
     password: "swordfish",
     confirmPassword: "sword",
   });
   submitForm();
+
   const passwordsMatchError = screen.queryByText(
     /the passwords don\'t match - try again/i
   );
@@ -147,9 +140,6 @@ test("7) confirm password should match password", () => {
 test("8) no error messages on all valid inputs", () => {
   //this is the "happy path" test - asserting no errors when everything done right
 
-  // userEvent.type(emailInputElement, "selena@gmail.com");
-  // userEvent.type(passwordInputElement, "swordfish");
-  // userEvent.type(confirmPasswordInputElement, "swordfish");
   typeIntoForm({
     email: "selena@gmail.com",
     password: "swordfish",
@@ -157,19 +147,15 @@ test("8) no error messages on all valid inputs", () => {
   });
   submitForm();
 
-  const emailErrorElement = screen.queryByText(
-    /the email you input is invalid/i
-  );
-
-  const passwordErrorElement = screen.queryByText(
-    /the password you entered should contain 5 or more characters/i
-  );
-
-  const passwordsMatchError = screen.queryByText(
-    /the passwords don\'t match - try again/i
-  );
-
-  expect(emailErrorElement).not.toBeInTheDocument();
-  expect(passwordErrorElement).not.toBeInTheDocument();
-  expect(passwordsMatchError).not.toBeInTheDocument();
+  expect(
+    screen.queryByText(/the email you input is invalid/i)
+  ).not.toBeInTheDocument();
+  expect(
+    screen.queryByText(
+      /the password you entered should contain 5 or more characters/i
+    )
+  ).not.toBeInTheDocument();
+  expect(
+    screen.queryByText(/the passwords don\'t match - try again/i)
+  ).not.toBeInTheDocument();
 });
